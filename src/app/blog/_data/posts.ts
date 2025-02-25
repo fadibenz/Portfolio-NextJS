553b338612da8c7db959bf9e2d53b67fc5cd5b22
fadi benzaima <f.benzaima@esi-sba.dz>
2025-02-25 10:01:47 +0100

    Removed palceholder blog

--- src/app/blog/_data/posts.ts
+++ src/app/blog/_data/posts.ts
@@ -1,16 +1,8 @@
 import { BlogPost } from "../_types/blog"
 
 export const blogPosts: BlogPost[] = [
-    {
-        id: 1,
-        title: "Basic ML Doctrine",
-        description: "A deep, yet intuitive understanding of the basic principles of Machine Learning",
-        date: "January 20, 2024",
-        readingTime: "10 min read",
-        slug: "Basic-ML-Doctrine",
-    },
         {
-        id: 2,
+        id: 1,
         title: "RNNs motivation and challenges",
         description: "A different perspective to Recurrent Neural Networks",
         date: "February 25, 2025",
